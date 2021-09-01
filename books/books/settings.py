a123e9af5474d94e982119d67b90a8b1fe12fd41
mwackowski <58736245+mwackowski@users.noreply.github.com>
2021-09-01 21:30:16 +0200

    Update settings.py

--- books/books/settings.py
+++ books/books/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5--6nb7l^fc61^03+lt@i87v(l_$9$b+o$cfpuft8m1qlt=8mb'
+SECRET_KEY = 'django-aaaa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
